b07824a9d6eacde2dd7038fe5c7493f17449f586
Andreas Moll <Andreas.Moll@synchrotron.org.au>
2017-01-18 17:29:43 +1100

    Fix wrong indentation

--- simplekv/db/consul.py
+++ simplekv/db/consul.py
@@ -21,32 +21,32 @@ class ConsulStore(KeyValueStore):
         index, keys = self.consul.kv.get('', keys=True)
         return keys
 
-     def iter_keys(self):
+    def iter_keys(self):
         return iter(self.keys())
 
-     def _has_key(self, key):
+    def _has_key(self, key):
         index, key = self.consul.kv.get(key, keys=True)
         return key is None
 
-     def _get(self, key):
+    def _get(self, key):
         index, val = self.consul.kv.get(key)
 
         if val is None:
             raise KeyError(key)
         return val['Value']
 
-     def _get_file(self, key, file):
+    def _get_file(self, key, file):
         file.write(self._get(key))
 
-     def _open(self, key):
+    def _open(self, key):
         return BytesIO(self._get(key))
 
-     def _put(self, key, value):
+    def _put(self, key, value):
         if not self.consul.kv.put(key, value):
             raise IOError('Failed to store data')
         return key
 
-     def _put_file(self, key, file):
+    def _put_file(self, key, file):
         self._put(key, file.read())
         return key
 
